1cf6e11d67b2ab607d8728d81dd33110a166c997
Jia-Joe <472167879@qq.com>
2015-08-09 21:09:50 +0800

    Strategy pattern

---
+++ OOP/Interface_1/interfaces/classprocessor/StringProcessor.java
@@ -0,0 +1,35 @@
+package interfaces.classprocessor;
+import java.util.*;
+
+public abstract class StringProcessor {
+
+	public String name() {
+		return getClass().getSimpleName();
+	}
+	public abstract String process(Object input);
+	
+	public static String s ="Hello Java";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Apply.process(new Upcase(),s);
+		Apply.process(new Downcase(),s);
+		Apply.process(new Splitter(),s);
+	}
+
+}
+
+class Upcase extends StringProcessor implements Processor{
+	public String process(Object input) {
+		return ((String)input).toUpperCase();
+	}
+}
+class Downcase extends StringProcessor implements Processor{
+	public String process(Object input) {
+		return ((String)input).toLowerCase();
+	}
+}
+class Splitter extends StringProcessor implements Processor{
+	public String process(Object input) {
+		return Arrays.toString(((String)input).split(" "));
+	}
+}
--- OOP/Interface_1/interfaces/filters/Waveform.java
+++ OOP/Interface_1/interfaces/filters/Waveform.java
@@ -1,10 +1,9 @@
 package interfaces.filters;
 
 public class Waveform {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	private static long counter;
+	private final long id=counter++;
+	public String toString()
+	{return "Waveform"+id;}
 }
+
