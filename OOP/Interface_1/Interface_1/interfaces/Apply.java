9191d9ee47fc58a653ccb80f51a78d934680a636
Jia-Joe <472167879@qq.com>
2015-08-09 13:43:25 +0800

    processor

---
+++ OOP/Interface_1/Interface_1/interfaces/Apply.java
@@ -0,0 +1,41 @@
+package Interface_1.interfaces;
+import java.util.*;
+
+class Processor {
+	public String name() {
+		return getClass().getSimpleName();
+	}
+	Object process(Object input) {
+		return input;
+	}
+}
+class Upcase extends Processor {
+	String process(Object input) {
+		return ((String)input).toUpperCase();
+	}
+}
+class Downcase extends Processor {
+	String process(Object input) {
+		return ((String)input).toLowerCase();
+	}
+}
+class Splitter extends Processor {
+	String process(Object input) {
+		return Arrays.toString(((String)input).split(" "));
+	}
+}
+public class Apply {
+	public static void process(Processor p,Object s) {
+		System.out.println("Using Processor"+p.name());
+		System.out.println(p.process(s));
+	}
+	public static String s ="Hello Java";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		process(new Upcase(),s);
+		process(new Downcase(),s);
+		process(new Splitter(),s);
+	}
+	
+
+}
