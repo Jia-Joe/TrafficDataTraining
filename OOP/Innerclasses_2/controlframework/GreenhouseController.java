bff9b3eedc6bfea230daa4a142044d076b96e5f3
Jia-Joe <472167879@qq.com>
2015-08-13 21:40:45 +0800

    Test Event

--- OOP/Innerclasses_2/controlframework/GreenhouseController.java
+++ OOP/Innerclasses_2/controlframework/GreenhouseController.java
@@ -4,7 +4,21 @@ public class GreenhouseController {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		GreenhouseControls gc=new GreenhouseControls();
+		gc.addEvent(gc.new Bell(3));
+		Event[] eventList= {
+				gc.new ThermostatNight(0),
+				gc.new LightOn(200),
+				gc.new LightOff(100),
+				gc.new WaterOn(800),
+				gc.new WaterOff(150),
+				gc.new ThermostatDay(1400)
+		};
+		gc.addEvent(gc.new Restart(2000, eventList));
+//		if(args.length==1)
+//			gc.addEvent(new GreenhouseControls.Terminate(new Integer(args[0])));
+		gc.addEvent(new GreenhouseControls.Terminate(200));
+		gc.run();
 	}
 
 }
