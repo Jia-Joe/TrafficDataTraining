559018d3630a1e7247b054c29b35b95d9bc876eb
Jia-Joe <472167879@qq.com>
2015-08-14 23:12:33 +0800

    DynamicFieldException

---
+++ OOP/Exceptions_3/exceptions/DynamicFields.java
@@ -0,0 +1,115 @@
+package exceptions;
+
+import java.nio.file.NoSuchFileException;
+
+class DynamicFieldsException extends Exception{}
+
+public class DynamicFields {
+	
+	private Object[][] fields;
+	
+	public DynamicFields(int initialSize) {
+		fields=new Object[initialSize][2];
+		for(int i=0;i<initialSize;i++)
+			fields[i]=new Object[]{null,null};
+	}
+	
+	public String toString() {
+		StringBuilder result=new StringBuilder();
+		for(Object[] obj:fields) {
+			result.append(obj[0]);
+			result.append(": ");
+			result.append(obj[1]);
+			result.append("\n");
+		}
+		return result.toString(); 
+	}
+
+	private int hasField(String id) {
+		for(int i=0;i<fields.length;i++) {
+			if(id.equals(fields[i][0]))
+				return i;
+		}
+		return -1;
+	}
+	
+	private int
+	getFieldNumber(String id) throws NoSuchFieldException {
+		int fieldNum=hasField(id);
+		if(fieldNum==-1)
+			throw new NoSuchFieldException();
+		return fieldNum;
+	}
+	private int makeField(String id) {
+		for(int i=0;i<fields.length;i++)
+			if(fields[i][0]==null) {
+				fields[i][0]=id;
+				return i;
+			}
+		//No empty fields,expand it
+		Object[][] tmp=new Object[fields.length+1][2];
+		for(int i=0;i<fields.length;i++) 
+			tmp[i]=fields[i];
+		for(int i=fields.length;i<tmp.length;i++)
+			tmp[i]=new Object[] {null,null};
+		fields=tmp;
+		return makeField(id);
+	}
+
+	public Object getField(String id) throws NoSuchFieldException {
+		return fields[getFieldNumber(id)][1];
+	}
+	
+	public Object setField(String id,Object value)
+	throws DynamicFieldsException {
+		if(value==null) {
+			DynamicFieldsException def=new DynamicFieldsException();
+			def.initCause(new NullPointerException());
+			throw def;
+		}
+		int fieldNumber=hasField(id);
+		if(fieldNumber==-1)
+			fieldNumber=makeField(id);
+		Object result=null;
+		try {
+			result=getField(id);
+		}catch(NoSuchFieldException e) {
+			throw new RuntimeException(e);
+		}
+		fields[fieldNumber][1]=value;
+		return result;
+	}
+	
+
+	public static void main(String[] args) {
+		DynamicFields df=new DynamicFields(3);
+		System.out.println(df);
+		try {
+			df.setField("d", "A value for d");
+			df.setField("number", 47);
+			df.setField("number2", 48);
+			System.out.println(df);
+			df.setField("d", "A new value for d");
+			df.setField("number3", 12);
+			System.out.println("df: "+df);
+			System.out.println("df.getField(\"d\"): "+df.getField("d"));
+			//Exception
+			Object field=df.setField("d", null);
+		}catch(NoSuchFieldException e) {
+			e.printStackTrace(System.out);
+		}catch(DynamicFieldsException e) {
+			e.printStackTrace(System.out);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
--- OOP/Innerclasses_2/controlframework/GreenhouseController.java
+++ OOP/Innerclasses_2/controlframework/GreenhouseController.java
@@ -5,7 +5,7 @@ public class GreenhouseController {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GreenhouseControls gc=new GreenhouseControls();
-		gc.addEvent(gc.new Bell(3));
+		//gc.addEvent(gc.new Bell(300));
 		Event[] eventList= {
 				gc.new ThermostatNight(0),
 				gc.new LightOn(200),
@@ -17,6 +17,8 @@ public class GreenhouseController {
 		gc.addEvent(gc.new Restart(2000, eventList));
 //		if(args.length==1)
 //			gc.addEvent(new GreenhouseControls.Terminate(new Integer(args[0])));
+//		gc.addEvent(new GreenhouseControls.Terminate(200));
+		gc.addEvent(gc.new Bell(200));
 		gc.addEvent(new GreenhouseControls.Terminate(200));
 		gc.run();
 	}
